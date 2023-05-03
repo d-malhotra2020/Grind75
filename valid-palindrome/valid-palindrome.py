374111987b5482404ce0acb9b31b7d868a439c3f
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-05-03 10:08:23 -0500

    Time: 98 ms (5.14%) | Memory: 16.8 MB (15.82%) - LeetSync

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -6,16 +6,14 @@ class Solution:
             while left < right and not self.isalphanumeric(s[left]):
                 left+=1
             while left < right and not self.isalphanumeric(s[right]):
-                right-=1
+                right -=1
             if s[left].lower() != s[right].lower():
                 return False
             else:
                 left+=1
                 right-=1
         return True
-                
-
     def isalphanumeric(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or
+        return (ord('A') <= ord(c) <= ord('Z') or 
                 ord('a') <= ord(c) <= ord('z') or
                 ord('0') <= ord(c) <= ord('9'))
