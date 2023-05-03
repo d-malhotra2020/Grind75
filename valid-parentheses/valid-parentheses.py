8e045298fdfb163354f72804560c168f5279bd0f
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-05-02 22:00:18 -0500

    Time: 50 ms (6.17%) | Memory: 16.3 MB (5.79%) - LeetSync

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hashmap = {"}": "{", "]":"[", ")":"("}
+        for characters in s:
+            if characters in hashmap:
+                if stack and stack[-1] == hashmap[characters]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(characters)
+        return True if not stack else False
+                    
