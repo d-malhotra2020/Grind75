1b6777b0764ac7274cc2f30a062fe3d7c25ee019
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-05-23 22:16:45 -0500

    Time: 60 ms (6.14%) | Memory: 16.3 MB (9.21%) - LeetSync

--- backspace-string-compare/backspace-string-compare.py
+++ backspace-string-compare/backspace-string-compare.py
@@ -4,18 +4,18 @@ class Solution:
         t_out = []
 
         for char in s:
-            if char == '#':
-                if s_out: 
+            if char == "#":
+                if s_out:
                     s_out.pop()
             else:
                 s_out.append(char)
         for char in t:
-            if char == '#':
-                if t_out: 
+            if char == "#":
+                if t_out:
                     t_out.pop()
             else:
                 t_out.append(char)
         if s_out == t_out:
             return True
         else:
-            return False
+            return 
