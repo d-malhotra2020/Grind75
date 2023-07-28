e66c83862b1a55ab860c76d9bf24e75c08e30fba
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-07-28 16:23:45 -0500

    Time: 224 ms (99.59%) | Memory: 20.9 MB (11.93%) - LeetSync

--- 186-reverse-words-in-a-string-ii/reverse-words-in-a-string-ii.py
+++ 186-reverse-words-in-a-string-ii/reverse-words-in-a-string-ii.py
@@ -1,5 +1,9 @@
 class Solution:
     def reverseWords(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        #reversing the entire array
         left = 0
         right = len(s)-1
         while left <= right:
@@ -10,19 +14,21 @@ class Solution:
         right = 0
         index = 0
         while right < len(s):
-            if s[right] != ' ':
+            if s[right] !=' ':
                 right+=1
             else:
                 index = right
                 right-=1
-                while left <= right:
+                while left<=right:
                     s[left], s[right] = s[right], s[left]
                     left+=1
                     right-=1
                 right = index+1
                 left = right
-        right -=1
+        right-=1
         while left <= right:
             s[left], s[right] = s[right], s[left]
             left+=1
             right-=1
+            
+        
