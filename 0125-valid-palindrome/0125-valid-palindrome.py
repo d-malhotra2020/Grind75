ecac5a5cde16fabf953de0701da59ed77d11e553
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-04 15:57:23 -0500

    Time: 58 ms (24.38%), Space: 17 MB (78.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -13,8 +13,7 @@ class Solution:
                 left+=1
                 right-=1
         return True
-            
     def isalphanumeric(self, c):
         return (ord('A') <= ord(c) <= ord('Z') or
-            ord('a') <= ord(c) <= ord('z') or
-            ord('0') <= ord(c) <= ord('9'))
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
