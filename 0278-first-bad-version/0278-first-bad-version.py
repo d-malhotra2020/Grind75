710efa4bf02aa548c87d4f1304ac4ec6ba1003b7
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-10 12:53:20 -0500

    Time: 40 ms (41.70%), Space: 16.4 MB (16.81%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -6,7 +6,7 @@ class Solution:
         first = 1
         end = n
         while first < end:
-            mid = (first + end)//2
+            mid = (first+end)//2
             if isBadVersion(mid):
                 end = mid
             else:
