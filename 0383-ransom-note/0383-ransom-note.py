c8b5d17cb55ef34c29287171f2851a15c953f30a
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-10 14:02:24 -0500

    Time: 89 ms (8.23%), Space: 16.9 MB (7.17%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,14 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNoteSorted = sorted(list(ransomNote))
-        magazineSorted = sorted(list(magazine))
+        ransom = sorted(list(ransomNote))
+        mag = sorted(list(magazine))
         
-        for char in magazineSorted:
-            if ransomNoteSorted and char in ransomNoteSorted[0]:
-                ransomNoteSorted.pop(0)
-        if ransomNoteSorted:
-                return False
+        for char in mag:
+            if ransom and char in ransom[0]:
+                ransom.pop(0)
+        if ransom:
+            return False
         else:
-                return True
-            
-            
+            return True
