4bab8cbb4f196c0f855d73bdd154e7dcf4c48b38
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-13 21:34:47 -0500

    Time: 45 ms (33.73%), Space: 16.2 MB (86.51%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -7,14 +7,13 @@ class Solution:
         b = b[::-1]
         
         for i in range(max(len(a), len(b))):
-            digitA = (ord(a[i]) - ord("0")) if i < len(a) else 0
-            digitB = (ord(b[i]) - ord("0")) if i < len(b) else 0
+            digitA = (ord(a[i])-ord("0")) if i < len(a) else 0
+            digitB = (ord(b[i])-ord("0")) if i < len(b) else 0
             
             total = digitA + digitB + carry
             char = str(total%2)
             result = char + result
-            carry = (total//2)
+            carry = (total)//2
         if carry:
             result = "1" + result
         return result
-        
