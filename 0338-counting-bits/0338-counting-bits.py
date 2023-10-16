926b91a0cf45ca477eb96da0ccf954c172ccb26f
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-16 12:42:48 -0500

    Time: 79 ms (43.39%), Space: 23.1 MB (55.00%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        answer = [0] * (n+1)
+        for i in range(1, n+1):
+            half = math.floor(i//2)
+            if(i%2==1):
+                answer[i] = answer[half]+1
+            else:
+                answer[i] = answer[half]
+        return answer
+        
