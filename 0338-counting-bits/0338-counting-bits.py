0b69bcf755ae6529261b0db63547c93c1800decb
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-16 12:47:37 -0500

    Time: 79 ms (43.39%), Space: 23.1 MB (17.77%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,11 +1,11 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        answer = [0] * (n+1)
+        result = [0] * (n+1)
         for i in range(1, n+1):
-            half = math.floor(i//2)
-            if(i%2==1):
-                answer[i] = answer[half]+1
+            halfposition = math.floor(i//2)
+            if (i%2 == 1):
+                result[i] = result[halfposition]+1
             else:
-                answer[i] = answer[half]
-        return answer
-        
+                result[i] = result[halfposition]
+        return result
+            
