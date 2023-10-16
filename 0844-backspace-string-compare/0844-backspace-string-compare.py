d9089789b840eaa5b8632d77d7dc9eab10ddf2c2
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-16 10:31:32 -0500

    Time: 45 ms (16.45%), Space: 16.1 MB (84.46%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,22 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s_output = []
+        t_output = []
+        
+        for char in s:
+            if char == "#":
+                if s_output:
+                    s_output.pop()
+            else:
+                s_output.append(char)
+        for char in t:
+            if char == "#":
+                if t_output:
+                    t_output.pop()
+            else:
+                t_output.append(char)
+        if s_output == t_output:
+            return True
+        else:
+            return False
+                
