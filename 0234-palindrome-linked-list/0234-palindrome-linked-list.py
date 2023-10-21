4255c298bb676f207aa2d529b97eefbae0c9db6a
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-20 22:04:11 -0500

    Time: 528 ms (83.02%), Space: 49.4 MB (11.87%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -13,11 +13,11 @@ class Solution:
             
         left = 0
         right = len(nums)-1
-            
+        
         while left <= right:
             if nums[left] != nums[right]:
                 return False
             left+=1
             right-=1
-                
         return True
+            
