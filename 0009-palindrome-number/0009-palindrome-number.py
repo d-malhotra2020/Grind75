159042b86c06ba6490637c7a2b9609a49dd8fcc0
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-22 14:29:46 -0500

    Time: 78 ms (8.20%), Space: 16.3 MB (17.49%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,16 +4,14 @@ class Solution:
             return False
         div = 1
         while x >= div*10:
-            div*=10
+            div *= 10
         while x:
-            right = x % 10
+            right = x%10
             left = x//div
-        
+            
             if left != right:
                 return False
-        
             x = (x%div)//10
-            div = div//100
-        
+            div = div // 100
         return True
-        
+            
