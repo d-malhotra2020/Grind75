4702c11a1b11fd564d802de4ff2d1a9871593011
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-26 12:00:28 -0500

    Time: 499 ms (86.16%), Space: 18.8 MB (96.60%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -11,13 +11,13 @@ class Solution:
                     q.append((i, j))
                 else:
                     mat[i][j] = "#"
-        for rows, columns in q:
+                    
+        for row, column in q:
             for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
-                nr = rows + dx
-                nc = columns + dy
+                nr = row + dx
+                nc = column + dy
                 
-                if 0 <= nr < height and 0 <= nc < width and mat[nr][nc] == '#':
-                    mat[nr][nc] = mat[rows][columns] +1
+                if 0 <= nr < height and 0 <= nc < width and mat[nr][nc] == "#":
+                    mat[nr][nc] = mat[row][column] + 1
                     q.append((nr, nc))
         return mat
-                    
