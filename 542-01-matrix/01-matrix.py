303970356ca78faab756d05bd34a03a92b29cbd9
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-10-26 12:37:04 -0500

    Time: 493 ms (88.38%) | Memory: 18.8 MB (96.60%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,6 +1,5 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        
         height = len(mat)
         width = len(mat[0])
         q = []
@@ -13,13 +12,11 @@ class Solution:
                     mat[i][j] = "#"
         
         for rows, columns in q:
-            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                nr = rows + dx
-                nc = columns + dy
-            
+            for dx, dy, in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr = dx + rows
+                nc = dy + columns
+                
                 if 0 <= nr < height and 0 <= nc < width and mat[nr][nc] == "#":
                     mat[nr][nc] = mat[rows][columns] + 1
                     q.append((nr, nc))
         return mat
-                
-            
