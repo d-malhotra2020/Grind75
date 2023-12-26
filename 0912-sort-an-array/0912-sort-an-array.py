359775b12b56eefa7b03d9f11939916bd0112ffe
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-12-26 13:10:11 -0600

    Time: 1151 ms (78.46%), Space: 24.8 MB (45.75%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,37 +1,33 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(arr, L, M, R):
-            left = arr[L:M+1]
-            right = arr[M+1:R+1]
-            i = L
+        self.mergeSort(nums)
+        return nums
+    def mergeSort(self, nums: List[int]):
+        if len(nums) > 1:
+            mid = (len(nums)//2)
+            left = nums[:mid]
+            right = nums[mid:]
+            
+            self.mergeSort(left)
+            self.mergeSort(right)
+            
+            i = 0
             j = 0
             k = 0
-            
-            while j < len(left) and k < len(right):
-                if left[j] <= right[k]:
-                    arr[i] = left[j]
-                    j+=1
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    nums[k] = left[i]
+                    i+=1
                 else:
-                    arr[i] = right[k]
-                    k+=1
-                i+=1
-            while j < len(left):
-                nums[i] = left[j]
-                j+=1
-                i+=1
-            while k < len(right):
-                nums[i] = right[k]
+                    nums[k] = right[j]
+                    j+=1
                 k+=1
+            while i < len(left):
+                nums[k] = left[i]
                 i+=1
-                
-                    
-        def mergeSort(arr, left, right):
-            if left == right:
-                return arr
-            mid = (left + right) // 2
-            mergeSort(arr, left, mid)
-            mergeSort(arr, mid+1, right)
-            merge(arr, left, mid, right)
-            return arr
-        return mergeSort(nums, 0, len(nums)-1)
-        
+                k+=1
+            while j < len(right):
+                nums[k] = right[j]
+                j+=1
+                k+=1
+            
