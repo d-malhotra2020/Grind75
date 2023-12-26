5a0ffd6c5dd73b7a18af2e3c65485f96bb3f8514
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2023-12-26 13:29:11 -0600

    Time: 1148 ms (78.50%), Space: 24.9 MB (45.75%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -4,30 +4,32 @@ class Solution:
         return nums
     def mergeSort(self, nums: List[int]):
         if len(nums) > 1:
+            
             mid = (len(nums)//2)
             left = nums[:mid]
             right = nums[mid:]
-            
+        
             self.mergeSort(left)
             self.mergeSort(right)
-            
-            i = 0
+        
+            i = 0 
             j = 0
             k = 0
+        
             while i < len(left) and j < len(right):
                 if left[i] < right[j]:
                     nums[k] = left[i]
-                    i+=1
+                    i+=1    
                 else:
                     nums[k] = right[j]
                     j+=1
                 k+=1
             while i < len(left):
-                nums[k] = left[i]
-                i+=1
-                k+=1
+                    nums[k] = left[i]
+                    i+=1
+                    k+=1
             while j < len(right):
-                nums[k] = right[j]
-                j+=1
-                k+=1
-            
+                    nums[k] = right[j]
+                    j+=1
+                    k+=1    
+        
