aa12a3e51aa651cd17eb919c000dcd56eb10ea9e
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-01-03 09:15:49 -0600

    Time: 42 ms (50.09%), Space: 17.1 MB (19.67%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -7,11 +7,11 @@ class Solution:
         right = len(nums) - 1
         i = 0
         
-        def swap(i, j):
+        def swap (i, j):
             tmp = nums[i]
             nums[i] = nums[j]
             nums[j] = tmp
-            
+        
         while i <= right:
             if nums[i] == 0:
                 swap(left, i)
@@ -21,4 +21,5 @@ class Solution:
                 right-=1
                 i-=1
             i+=1
-        
+            
+            
