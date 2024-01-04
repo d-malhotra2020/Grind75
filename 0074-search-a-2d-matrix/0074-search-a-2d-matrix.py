8a1d47a9e9671dd4c35b88309d2b71f8c078a19c
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-01-04 08:42:08 -0600

    Time: 51 ms (49.88%), Space: 17.9 MB (12.70%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -7,23 +7,22 @@ class Solution:
         top = 0
         bottom = row - 1
         
-        while top <= bottom:
-            row = (top + bottom) // 2
+        while (top <= bottom):
+            row = (top + bottom)//2
             if target > matrix[row][-1]:
                 top = row + 1
             elif target < matrix[row][0]:
                 bottom = row - 1
             else:
                 break
-        if not (top<=bottom):
+        if not (top <= bottom):
             return False
         
-        row = (top + bottom)//2
+        row = (top + bottom) // 2
         left = 0
         right = columns
-        
-        while left <= right:
-            mid = (left+right)//2
+        while (left <= right):
+            mid = (left + right)//2
             if target > matrix[row][mid]:
                 left = mid + 1
             elif target < matrix[row][mid]:
@@ -31,4 +30,3 @@ class Solution:
             else:
                 return True
         return False
-        
