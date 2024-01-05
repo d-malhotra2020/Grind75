e7023190cee6ccb5840bd6a6181598d7da414650
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-01-05 10:11:29 -0600

    Time: 288 ms (87.72%), Space: 19 MB (8.26%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -7,13 +7,12 @@ class Solution:
         
         while left <= right:
             k = (left + right)//2
-            hours = 0 
+            hours = 0
             for p in piles:
-                hours += math.ceil(p / k)
-                
-            if hours <=h:
+                hours += math.ceil(p/k)
+            if hours<=h:
                 result = min(result, k)
-                right = k - 1
+                right = k -1
             else:
                 left = k + 1
         return result
