fb950300e646b5a5d363b904a4e29e47ede8891e
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-02-05 13:18:11 -0600

    Time: 39 ms (80.13%), Space: 17.4 MB (96.42%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -12,7 +12,5 @@ class Solution:
             currentSum += node.val
             if not node.left and not node.right:
                 return currentSum == targetSum
-            return (dfs(node.left, currentSum) or dfs(node.right, currentSum))
+            return (dfs(node.right, currentSum) or dfs(node.left, currentSum))
         return dfs(root, 0)
-            
-            
