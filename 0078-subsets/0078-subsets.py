12c9056cdc75f54968803dedc48a1e803a382717
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-02-07 14:38:38 -0600

    Time: 43 ms (31.79%), Space: 16.6 MB (78.88%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,5 +1,6 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        
         result = []
         subset = []
         
@@ -8,10 +9,12 @@ class Solution:
                 result.append(subset.copy())
                 return
             
+            
             subset.append(nums[index])
             dfs(index+1)
             
             subset.pop()
             dfs(index+1)
+            
         dfs(0)
         return result
