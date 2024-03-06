ec168179940106daeb1572601149ddc80826eb62
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-03-06 10:19:07 -0600

    Time: 39 ms (46.12%) | Memory: 16.6 MB (98.52%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
-        subset = []
+        current = []
         def dfs(index):
             if index >= len(nums):
-                result.append(subset.copy())
+                result.append(current.copy())
                 return
+            
             #decision to include nums[index]
-            subset.append(nums[index])
+            current.append(nums[index])
+            dfs(index+1)
+            current.pop()
             dfs(index+1)
             
-            #decision NOT to include nums[index]
-            subset.pop()
-            dfs(index+1)
         dfs(0)
         return result
+            
