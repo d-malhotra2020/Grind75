d4fd89cc3c5805ad1a3e433bed31e148d379eda0
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-07-03 10:29:23 -0500

    Time: 1154 ms (45.95%) | Memory: 23.9 MB (85.26%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -7,12 +7,12 @@ class Solution:
             mid = (len(nums)//2)
             left = nums[:mid]
             right = nums[mid:]
-            
+        
             self.mergeSort(left)
             self.mergeSort(right)
-            
-            i = j = k = 0
-            
+            i = 0
+            j = 0
+            k = 0
             while i < len(left) and j < len(right):
                 if left[i] < right[j]:
                     nums[k] = left[i]
@@ -29,8 +29,5 @@ class Solution:
                 nums[k] = right[j]
                 j+=1
                 k+=1
-                
-
-            
-    
-                
+        
+        
