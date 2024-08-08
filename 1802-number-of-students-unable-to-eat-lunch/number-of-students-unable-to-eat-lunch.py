1a39f386c4a226eb29437305c3fb953db746bd95
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-08-07 20:50:18 -0500

    Time: 38 ms (71.17%) | Memory: 16.5 MB (34.15%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,15 +1,15 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        NumberOfSandwiches = 0
-        while students and sandwiches:
+        sandwichcounter = 0
+        while sandwiches and students:
             if students[0] == sandwiches[0]:
                 sandwiches.pop(0)
                 students.pop(0)
-                NumberOfSandwiches = 0
+                sandwichcounter = 0
             else:
                 student = students.pop(0)
                 students.append(student)
-                NumberOfSandwiches +=1
-                if NumberOfSandwiches == len(students):
+                sandwichcounter +=1
+                if sandwichcounter == len(students):
                     break
         return len(students)
