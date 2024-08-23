8e47b13c37efb5cb79182318bc3686eac03f4a67
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-08-23 09:39:36 -0500

    Time: 45 ms (6.56%) | Memory: 16.6 MB (18.47%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,7 +6,6 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        
         current = root
         stack = []
         result = []
