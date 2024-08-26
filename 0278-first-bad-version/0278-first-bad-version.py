06220d342afefb80311c0113803d1347617631eb
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-08-26 14:22:31 -0500

    Time: 34 ms (59.18%), Space: 16.5 MB (15.52%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -5,11 +5,11 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         first = 1
         end = n
-        
         while (first < end):
-            mid = (first+end)//2
+            mid = (first + end)//2
             if isBadVersion(mid):
                 end = mid
             else:
                 first = mid + 1
         return first
+        
