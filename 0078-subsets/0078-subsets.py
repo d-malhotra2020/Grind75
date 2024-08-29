f9dcefdb834bb97cce2994b451e31818ebcef6b3
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-08-29 14:53:25 -0500

    Time: 36 ms (63.09%), Space: 16.7 MB (67.13%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,18 +2,15 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
         subset = []
-        
-        def dfs(index):
-            if index >= len(nums):
+        def dfs(i):
+            if i >= len(nums):
                 result.append(subset.copy())
                 return
             
-            subset.append(nums[index])
-            dfs(index+1)
-            
+            subset.append(nums[i])
+            dfs(i+1)
             subset.pop()
-            dfs(index+1)
+            dfs(i+1)
             
         dfs(0)
         return result
-            
