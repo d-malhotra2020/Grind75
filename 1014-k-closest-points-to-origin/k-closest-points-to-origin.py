92aecfa59fc4e6bd955c12d75948572d1c9adf1a
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-02 20:32:38 -0500

    Time: 616 ms (33.13%) | Memory: 23.1 MB (21.51%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -4,11 +4,10 @@ class Solution:
         for x, y in points:
             distance = (x**2) + (y**2)
             minHeap.append([distance, x, y])
-        
         heapq.heapify(minHeap)
         result = []
         while k > 0:
             distance, x, y = heapq.heappop(minHeap)
             result.append([x, y])
-            k -=1
+            k-=1
         return result
