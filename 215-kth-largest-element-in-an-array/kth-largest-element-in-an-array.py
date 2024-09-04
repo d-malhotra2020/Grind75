faa741502ac9239f06ef43fd3be5caf93d1fab9f
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-04 15:13:34 -0500

    Time: 1203 ms (5.01%) | Memory: 37.3 MB (5.70%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -11,13 +11,14 @@ class Solution:
                 if nums[i] < pivot:
                     nums[p], nums[i] = nums[i], nums[p]
                     p+=1
-            nums[p], nums[right] = nums[right], nums[p]
+            nums[right], nums[p] = nums[p], nums[right]
             
-            if p > k: 
+            if p > k:
                 return quickSelect(left, p-1)
             elif p < k:
                 return quickSelect(p+1, right)
             else:
                 return nums[p]
+        
         return quickSelect(0, len(nums) - 1)
-                
+        
