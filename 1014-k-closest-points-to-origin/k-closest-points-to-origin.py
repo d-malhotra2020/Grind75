662bd66491c1822de9be36bb88b679531d00037b
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-05 12:50:13 -0500

    Time: 607 ms (45.43%) | Memory: 23.1 MB (42.30%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,7 +1,7 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        result = []
         minHeap = []
+        result = []
         for x, y in points:
             distance = (x**2) + (y**2)
             minHeap.append([distance, x, y])
@@ -11,4 +11,3 @@ class Solution:
             result.append([x, y])
             k-=1
         return result
-            
