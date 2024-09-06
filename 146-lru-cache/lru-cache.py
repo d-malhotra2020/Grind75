eee6904e4122b536c22a271694bac391afe155c9
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-05 21:39:40 -0500

    Time: 584 ms (40.05%) | Memory: 78.1 MB (66.21%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,29 +1,50 @@
-from collections import deque
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.c = capacity
-        self.m = dict()
-        self.deq = deque()
-
+        self.cap = capacity
+        self.cache = {}
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+        
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+        prev.next = node
+        nxt.prev = node
+        node.next = nxt
+        node.prev = prev
+        
     def get(self, key: int) -> int:
-        if key in self.m:
-            value = self.m[key]
-            self.deq.remove(key)
-            self.deq.append(key)
-            return value
-        else:
-            return -1
-
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+    
     def put(self, key: int, value: int) -> None:
-        if key not in self.m:
-            if len(self.deq) == self.c:
-                oldest = self.deq.popleft()
-                del self.m[oldest]
-        else:
-            self.deq.remove(key)
-        self.m[key] = value
-        self.deq.append(key)
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+        
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
