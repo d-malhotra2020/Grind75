d8b1444e8753b9eda1e85b8ce5e93073d1317de6
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-12 11:27:42 -0500

    Time: 607 ms (17.83%) | Memory: 78 MB (72.75%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -4,6 +4,7 @@ class Node:
         self.val = val
         self.prev = None
         self.next = None
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -13,13 +14,12 @@ class LRUCache:
         self.right = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
-    
     def remove(self, node):
         nextNode = node.next
         prevNode = node.prev
-        nextNode.prev = prevNode
         prevNode.next = nextNode
-    
+        nextNode.prev = prevNode
+        
     def insert(self, node):
         prevNode = self.right.prev
         nextNode = self.right
@@ -27,20 +27,20 @@ class LRUCache:
         nextNode.prev = node
         node.next = nextNode
         node.prev = prevNode
-        
+
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-    
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-        
+            
         if len(self.cache) > self.capacity:
             LRU = self.left.next
             self.remove(LRU)
