f66ec1e3905a4b856715cd2ab96fc52201151bdc
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-12 20:50:07 -0500

    Time: 239 ms (66.68%), Space: 18.7 MB (95.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -11,22 +11,17 @@ class Solution:
                     self.dfs(grid, r, c)
                     islands +=1
         return islands
-                    
-                    
-                    
-                    
+    
+    
     def dfs(self, grid, r, c):
         rows = len(grid)
         columns = len(grid[0])
         
         if r < 0 or c < 0 or r >= rows or c >= columns or grid[r][c] != "1":
             return
-        
         grid[r][c] = "#"
         self.dfs(grid, r+1, c)
         self.dfs(grid, r-1, c)
         self.dfs(grid, r, c+1)
         self.dfs(grid, r, c-1)
-            
-        
-                    
+                
