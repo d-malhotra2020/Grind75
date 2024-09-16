e5fc3607bbcbadcf4eac2d0414d3d790b4d36a66
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-16 11:02:22 -0500

    Time: 412 ms (76.36%), Space: 18.2 MB (36.82%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -3,19 +3,18 @@ class Solution:
         rows = len(grid)
         q = deque([(0, 0, 1)])
         visit = set((0, 0))
-        direction = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [1, -1], [-1, -1]]
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
         
         while q:
             r, c, length = q.popleft()
             if r < 0 or c < 0 or r >= rows or c >= rows or grid[r][c] == 1:
                 continue
-            if (r == rows - 1 and c == rows - 1):
+            if (r == rows - 1 and c == rows -1):
                 return length
-            for dr, dc in direction:
+            for dr, dc in directions:
                 row = r + dr
                 col = c + dc
-                if ((row, col)) not in visit:
+                if (row, col) not in visit:
                     q.append((row, col, length+1))
                     visit.add((row, col))
-                    
         return -1
