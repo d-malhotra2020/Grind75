e207ea13c8e6aea2edfa00ca0a580f523f1881c9
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-15 20:02:34 -0500

    Time: 52 ms (44.85%), Space: 16.5 MB (58.96%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -11,7 +11,8 @@ class Solution:
                 if grid[r][c] == 1:
                     fresh +=1
                 if grid[r][c] == 2:
-                   q.append([r, c]) 
+                    q.append([r, c])
+                    
         directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         while q and fresh > 0:
             for i in range(len(q)):
@@ -26,6 +27,3 @@ class Solution:
                     fresh -=1
             time +=1
         return time if fresh == 0 else -1
-                        
-            
-            
