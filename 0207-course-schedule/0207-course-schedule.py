0ccd7d8c250ebf6f58d86419d89dcaa93b947119
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-17 12:40:17 -0500

    Time: 108 ms (8.73%), Space: 18.6 MB (11.99%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -6,22 +6,25 @@ class Solution:
         
         for course, prereq in prerequisites:
             hashmap[course].append(prereq)
-            
+        
         def dfs(course):
             if course in visit:
                 return False
             if hashmap[course] == []:
                 return True
+            
             visit.add(course)
             
             for prereq in hashmap[course]:
                 if not dfs(prereq):
                     return False
+                
             visit.remove(course)
             hashmap[course] = []
             return True
-        
+            
         for course in range(numCourses):
             if not dfs(course):
                 return False
         return True
+                
