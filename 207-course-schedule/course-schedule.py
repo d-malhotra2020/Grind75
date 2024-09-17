82d3bc17cc0390b44b8d242eb227df29ec25145d
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-17 10:22:19 -0500

    Time: 104 ms (10.28%) | Memory: 18.6 MB (11.99%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,21 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         hashmap = {i:[] for i in range(numCourses)}
+        visit = set()
         
         for course, prereq in prerequisites:
             hashmap[course].append(prereq)
-            
-        visit = set()
+        
         def dfs(course):
             if course in visit:
                 return False
             if hashmap[course] == []:
                 return True
             visit.add(course)
+            
             for prereq in hashmap[course]:
-                if not  dfs(prereq): 
+                if not dfs(prereq):
                     return False
-                
             visit.remove(course)
             hashmap[course] = []
             return True
@@ -23,5 +23,3 @@ class Solution:
             if not dfs(course):
                 return False
         return True
-        
-        
