685cc7cdc3f6fd13170900a9fa60a52079ce743c
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-18 16:14:59 -0500

    Time: 41 ms (76.02%), Space: 16.6 MB (53.29%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,12 +3,14 @@ class Solution:
         rows = len(obstacleGrid)
         columns = len(obstacleGrid[0])
         dp = [0] * columns
-        dp[columns-1] = 1
+        dp[columns - 1] = 1
         
         for r in reversed(range(rows)):
             for c in reversed(range(columns)):
                 if obstacleGrid[r][c]:
                     dp[c] = 0
-                elif c + 1 < columns:
+                elif c+1 < columns:
                     dp[c] = dp[c] + dp[c+1]
         return dp[0]
+                
+                
