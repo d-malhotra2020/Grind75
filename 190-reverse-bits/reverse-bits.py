a318d6abeafbc12d18607fe45c8fb4b137c78cb6
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-20 13:22:57 -0500

    Time: 35 ms (66.55%) | Memory: 16.7 MB (6.46%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -5,5 +5,5 @@ class Solution:
         
         for i in range(32):
             bit = (n >> i) & 1
-            result = result | (bit << (31 - i)) 
+            result = result | (bit << (31 - i))
         return result
