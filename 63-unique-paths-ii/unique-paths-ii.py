3c24a3e75a44d8c2cd8cc79bebca8d8b25130428
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-19 22:50:11 -0500

    Time: 39 ms (84.40%) | Memory: 16.6 MB (53.48%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -3,7 +3,7 @@ class Solution:
         rows = len(obstacleGrid)
         columns = len(obstacleGrid[0])
         dp = [0] * columns
-        dp[columns-1] = 1
+        dp[columns - 1] = 1
         
         for r in reversed(range(rows)):
             for c in reversed(range(columns)):
@@ -12,3 +12,4 @@ class Solution:
                 elif c + 1 < columns:
                     dp[c] = dp[c] + dp[c+1]
         return dp[0]
+        
