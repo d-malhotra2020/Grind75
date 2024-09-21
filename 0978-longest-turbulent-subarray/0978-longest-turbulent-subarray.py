966f039451f80f5305efdd0c1ac8ccb5bb8d195e
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-21 18:39:54 -0500

    Time: 365 ms (33.93%), Space: 20.5 MB (33.14%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -6,17 +6,16 @@ class Solution:
         prev = ""
         
         while right < len(arr):
-            if arr[right-1] > arr[right] and prev != ">":
+            if arr[right] > arr[right-1] and prev != ">":
                 result = max(result, right - left + 1)
-                prev = ">"
-                right +=1
-            elif arr[right-1] < arr[right] and prev != "<":
-                result = max(result, right - left + 1)
-                prev = "<"
                 right += 1
+                prev = ">"
+            elif arr[right] < arr[right-1] and prev != "<":
+                result = max(result, right - left + 1)
+                right +=1
+                prev = "<"
             else:
                 right = right + 1 if arr[right] == arr[right-1] else right
                 left = right - 1
                 prev = ""
         return result
-                
