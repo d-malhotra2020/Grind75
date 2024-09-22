aa050f7900def5580591da606bc2e411be1bdf44
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2024-09-22 17:40:23 -0500

    Time: 445 ms (70.97%), Space: 29.8 MB (38.14%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -3,10 +3,9 @@ class Solution:
         result = 0
         currentSum = sum(arr[:k-1])
         
-        for left in range(len(arr) - k + 1):
+        for left in range (len(arr) - k + 1):
             currentSum += arr[left + k - 1]
-            if (currentSum / k) >= threshold:
-                result +=1 
+            if (currentSum / k) >=threshold:
+                result += 1
             currentSum -= arr[left]
         return result
-            
