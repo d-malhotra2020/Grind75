c5c5842fe10ed465ab67ac6c2ca572485c3980f5
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2025-06-11 09:28:18 -0500

    Time: 24 ms (6.56%) | Memory: 17.8 MB (95.20%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -6,14 +6,15 @@ class Solution:
           while left < right and not self.alphaNumeric(s[left]):
             left += 1
           while right > left and not self.alphaNumeric(s[right]):
-            right -=1
+            right -= 1
           if s[left].lower() != s[right].lower():
             return False
-          left +=1
-          right -=1
+          left += 1
+          right -= 1
         return True
 
     def alphaNumeric(self, c):
       return (ord('A') <= ord(c) <= ord('Z') or
-            ord('a') <= ord(c) <= ord('z') or
-            ord('0') <= ord(c) <= ord('9'))
+      ord('a') <= ord(c) <= ord('z') or 
+      ord('0') <= ord(c) <= ord('9'))
+    
