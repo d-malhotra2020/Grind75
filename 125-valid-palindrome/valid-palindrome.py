e6500017a83007ed9767335d06ec179ee064c1fa
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2025-06-11 11:37:01 -0500

    Time: 17 ms (9.81%) | Memory: 18 MB (83.28%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,18 +3,19 @@ class Solution:
         left = 0
         right = len(s) - 1
         while left < right:
-          while left < right and not self.alphaNumeric(s[left]):
+          while left < right and not self.isAlphanumeric(s[left]):
             left += 1
-          while right > left and not self.alphaNumeric(s[right]):
+          while right > left and not self.isAlphanumeric(s[right]):
             right -= 1
           if s[left].lower() != s[right].lower():
             return False
           left += 1
           right -= 1
         return True
+    
+    def isAlphanumeric(self, c):
+      return (ord('A') <= ord(c) <= ord('Z') or 
+              (ord('a') <= ord(c) <= ord('z') or 
+              (ord('0') <= ord(c) <= ord('9'))))
 
-    def alphaNumeric(self, c):
-      return (ord('A') <= ord(c) <= ord('Z') or
-      ord('a') <= ord(c) <= ord('z') or 
-      ord('0') <= ord(c) <= ord('9'))
     
