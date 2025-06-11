400c446f5fe7d239554af954588989f2e6da9c7f
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2025-06-11 09:36:52 -0500

    Time: 99 ms (46.91%) | Memory: 26.9 MB (33.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -6,9 +6,9 @@ class Solution:
         while right < len(prices):
           if prices[left] < prices[right]:
             profit = prices[right] - prices[left]
-            maxProfit = max(maxProfit, profit)  
+            maxProfit = max(maxProfit, profit)
           else:
             left = right
-          right +=1
+          right = right + 1
         return maxProfit
-
+          
