2afee20fbaee1bfa78cbf0332135c37e3540599f
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2025-06-17 11:06:32 -0500

    Time: 11 ms (72.81%) | Memory: 17.8 MB (86.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,11 +5,11 @@ class Solution:
         
         sCount = {}
         tCount = {}
-
+        
         for i in range(len(s)):
           sCount[s[i]] = 1 + sCount.get(s[i], 0)
           tCount[t[i]] = 1 + tCount.get(t[i], 0)
-        
+
         for c in sCount:
           if sCount[c] != tCount.get(c, 0):
             return False
