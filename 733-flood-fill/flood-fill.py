2811b9ac724b29b0a4b31b89b19f50e6a21e7e81
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2025-06-23 08:34:33 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (51.38%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -3,12 +3,14 @@ class Solution:
         newColor = image[sr][sc]
         height = len(image)
         width = len(image[0])
+        
         def dfs(sr, sc):
-            if 0 <= sr < height and 0 <= sc < width and image[sr][sc] == newColor and image[sr][sc] != color:
-                image[sr][sc] = color
-                dfs(sr+1, sc)
-                dfs(sr-1, sc)
-                dfs(sr, sc+1)
-                dfs(sr, sc-1)
-        dfs(sr,sc)
+          if 0 <= sr < height and 0 <= sc < width and image[sr][sc] == newColor and image[sr][sc] != color:
+            image[sr][sc] = color
+            dfs(sr + 1, sc)
+            dfs(sr - 1, sc)
+            dfs(sr, sc + 1)
+            dfs(sr, sc - 1)
+        dfs(sr, sc)
         return image
+
