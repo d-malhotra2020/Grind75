181da1c62c72bfd259a277d2c9230b293dac8d23
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2025-12-16 11:49:53 -0600

    Time: 115 ms (80.80%) | Memory: 78.2 MB (22.60%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,52 +1,23 @@
-class Node:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
-        
+from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}
-        self.left = Node(0,0)
-        self.right = Node(0,0)
-        self.left.next = self.right
-        self.right.prev = self.left
-        
-    def remove(self, node):
-        nextNode = node.next
-        prevNode = node.prev
-        prevNode.next = nextNode
-        nextNode.prev = prevNode
-        
-    def insert(self, node):
-        prevNode = self.right.prev
-        nextNode = self.right
-        nextNode.prev = node
-        prevNode.next = node
-        node.next = nextNode
-        node.prev = prevNode
-        
+      self.capacity = capacity
+      self.cache = OrderedDict()
+             
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
+      if key not in self.cache:
         return -1
-       
+      
+      self.cache.move_to_end(key)
+      return self.cache[key]
+
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-        
-        if len(self.cache) > self.capacity:
-            LRU = self.left.next
-            self.remove(LRU)
-            del self.cache[LRU.key]
-        
+      if key in self.cache:
+        self.cache.move_to_end(key)
+      self.cache[key] = value
+      if len(self.cache) > self.capacity:
+        self.cache.popitem(last=False)
         
 
 
