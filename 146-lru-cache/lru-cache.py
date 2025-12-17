ab4f9cd75f07412e7cc73e3a0b47dc3bd5f92457
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2025-12-17 08:42:14 -0600

    Time: 106 ms (87.82%) | Memory: 78 MB (42.35%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -3,8 +3,7 @@ class LRUCache:
 
     def __init__(self, capacity: int):
       self.capacity = capacity
-      self.cache = OrderedDict()
-        
+      self.cache = OrderedDict()        
 
     def get(self, key: int) -> int:
       if key not in self.cache:
@@ -15,10 +14,9 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
       if key in self.cache:
         self.cache.move_to_end(key)
-      
       self.cache[key] = value
       if len(self.cache) > self.capacity:
-        self.cache.popitem(last = False)
+        self.cache.popitem(last=False)
         
 
 
