46b0826201f5c58366a01cb7d73fb165d85db0b4
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2025-12-30 09:43:55 -0600

    Time: 0 ms (100.00%) | Memory: 20.2 MB (99.33%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,11 +1,10 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-      alreadyMerged = []
-      intervals.sort(key = lambda x: x[0])
-      for i in intervals:
-        if not alreadyMerged or i[0] > alreadyMerged[-1][1]:
-          alreadyMerged.append(i)
-        else:
-          alreadyMerged[-1][1] = max(alreadyMerged[-1][1], i[-1])
-      return alreadyMerged
-        
+        alreadyMerged = []
+        intervals.sort(key = lambda x: x[0])
+        for i in intervals:
+          if not alreadyMerged or i[0] > alreadyMerged[-1][1]:
+            alreadyMerged.append(i)
+          else:
+            alreadyMerged[-1][1] = max(alreadyMerged[-1][1], i[1])
+        return alreadyMerged
