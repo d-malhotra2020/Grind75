2e9aed3f52119162577b49236b9a08ff0a053bd8
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-02 12:10:16 -0600

    Time: 3 ms (75.96%) | Memory: 18.6 MB (89.80%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -3,16 +3,15 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = {i: [] for i in range(numCourses)}
         indegree = [0] * numCourses
-
         for a, b in prerequisites:
           graph[b].append(a)
           indegree[a] += 1
+
         queue = deque()
-        
         for i in range(numCourses):
           if indegree[i] == 0:
             queue.append(i)
-        
+            
         order = []
         while queue:
           course = queue.popleft()
@@ -21,5 +20,7 @@ class Solution:
             indegree[neighbor] -= 1
             if indegree[neighbor] == 0:
               queue.append(neighbor)
+
         return order if len(order) == numCourses else []
 
+        
