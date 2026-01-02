376243c3d27679db0e01ab23e02cb3fb0cf0f043
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-02 12:17:45 -0600

    Time: 3 ms (75.96%) | Memory: 18.6 MB (89.80%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -8,19 +8,19 @@ class Solution:
           indegree[a] += 1
 
         queue = deque()
+
         for i in range(numCourses):
           if indegree[i] == 0:
             queue.append(i)
-            
+
         order = []
         while queue:
           course = queue.popleft()
           order.append(course)
           for neighbor in graph[course]:
-            indegree[neighbor] -= 1
+            indegree[neighbor] -=1
             if indegree[neighbor] == 0:
               queue.append(neighbor)
-
         return order if len(order) == numCourses else []
 
-        
+
