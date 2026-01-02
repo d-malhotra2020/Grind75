5a28300e53f3a2ef6ecdbcf2a1c5998798f2485f
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-02 13:11:39 -0600

    Time: 4 ms (46.88%) | Memory: 18.4 MB (94.99%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,19 +1,19 @@
 from collections import deque
-
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = {i: [] for i in range(numCourses)}
         prerequisiteCount = [0] * numCourses
-        
+
         for a, b in prerequisites:
           graph[b].append(a)
           prerequisiteCount[a] += 1
-
+        
         queue = deque()
         for i in range(numCourses):
           if prerequisiteCount[i] == 0:
             queue.append(i)
-        
+
+
         order = []
         while queue:
           course = queue.popleft()
@@ -24,4 +24,4 @@ class Solution:
               queue.append(neighbor)
         return order if len(order) == numCourses else []
 
-        
+
