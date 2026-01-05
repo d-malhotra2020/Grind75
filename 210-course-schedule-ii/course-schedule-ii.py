9c6737bf6d5190d8c20288d6a8f068f305973679
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-05 07:44:20 -0600

    Time: 3 ms (75.73%) | Memory: 18.3 MB (99.14%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -7,21 +7,24 @@ class Solution:
         for a, b in prerequisites:
           graph[b].append(a)
           prerequisiteCount[a] += 1
-
+        
         queue = deque()
         for i in range(numCourses):
           if prerequisiteCount[i] == 0:
             queue.append(i)
+        
         
         order = []
         while queue:
           course = queue.popleft()
           order.append(course)
           for neighbor in graph[course]:
-            prerequisiteCount[neighbor] -=1
+            prerequisiteCount[neighbor] -= 1
             if prerequisiteCount[neighbor] == 0:
               queue.append(neighbor)
         return order if len(order) == numCourses else []
+
+
 
 
 
