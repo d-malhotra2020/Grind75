1f63b30c014a067103e2c7e4becbe3dfc7bee5de
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-12 16:36:55 -0600

    Time: 0 ms (100.00%) | Memory: 21.5 MB (11.47%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -3,7 +3,7 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         if not intervals:
           return 0
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort(key = lambda x:x[0])
         min_heap = []
         for start, end in intervals:
           if min_heap and start >= min_heap[0]:
