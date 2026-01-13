89ed885cc52d7675a33277355f77a188f60a8b95
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-13 09:40:40 -0600

    Time: 173 ms (11.54%) | Memory: 69.8 MB (92.61%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,26 +1,27 @@
 class TimeMap:
 
     def __init__(self):
-        self.store = {}
+      self.store = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
       if key not in self.store:
         self.store[key] = []
       self.store[key].append([timestamp, value])
-
+        
     def get(self, key: str, timestamp: int) -> str:
-      results = ""
-      values = self.store.get(key, [])
+      result = ""
+      value = self.store.get(key, [])
       left = 0
-      right = len(values) - 1
+      right = len(value) - 1
       while left <= right:
         mid = (left + right)//2
-        if values[mid][0] <= timestamp:
-          results = values[mid][1]
+        if value[mid][0] <= timestamp:
+          result = value[mid][1]
           left = mid + 1
         else:
           right = mid - 1
-      return results 
+      return result
+      
         
 
 
