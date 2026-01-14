3be4acd18419d3ab187f0bc5918a2c0bf23f68f1
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-14 08:38:23 -0600

    Time: 241 ms (64.32%) | Memory: 21.5 MB (39.97%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,35 +1,25 @@
-from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
           return 0
-
         rows = len(grid)
-        cols = len(grid[0])
-        visit = set()
+        columns = len(grid[0])
         islands = 0
 
-        def bfs(r, c):
-          q = deque()
-          visit.add((r, c))
-          q.append((r, c))
-
-          while q:
-            row, col = q.popleft()
-            directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
-            for dr, dc in directions:
-              nr, nc = row + dr, col + dc
-              if ((0 <= nr < rows and
-                  (0 <= nc < cols and
-                  grid[nr][nc] == "1" and
-                  (nr, nc) not in visit))):
-                  q.append((nr, nc))
-                  visit.add((nr, nc))
+        def dfs(r, c):
+          if r < 0 or c < 0 or r >= rows or c >= columns:
+            return 
+          if grid[r][c] != "1":
+            return
+          grid[r][c] = "0"
+          dfs(r+1, c)
+          dfs(r-1, c)
+          dfs(r, c+1)
+          dfs(r, c-1)
 
         for r in range(rows):
-          for c in range(cols):
-            if grid[r][c] == "1" and (r,c) not in visit:
-              bfs(r, c)
+          for c in range(columns):
+            if grid[r][c] == "1":
+              dfs(r, c)
               islands += 1
         return islands
-        
