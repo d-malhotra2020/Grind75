66ed222abd8f38887f18fac8307eca54fe489384
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-15 12:42:26 -0600

    Time: 0 ms (100.00%) | Memory: 20.5 MB (9.55%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,25 +1,29 @@
 from collections import deque
-
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-      graph = {i: [] for i in range(numCourses)}
-      prerequisiteCount = [0] * numCourses
+        graph = {i: [] for i in range(numCourses)}
+        prerequisiteCount = [0] * numCourses
 
-      for a, b in prerequisites:
-        graph[b].append(a)
-        prerequisiteCount[a] += 1
+        for a, b in prerequisites:
+          graph[b].append(a)
+          prerequisiteCount[a] += 1      
+        
+        queue = deque()
 
-      queue = deque()
-      for i in range(numCourses):
-        if prerequisiteCount[i] == 0:
-          queue.append(i)
+        for i in range(numCourses):
+          if prerequisiteCount[i] == 0:
+            queue.append(i)
 
-      order = []
-      while queue:
-        course = queue.popleft()
-        order.append(course)
-        for neighbor in graph[course]:
-          prerequisiteCount[neighbor] -= 1
-          if prerequisiteCount[neighbor] == 0:
-            queue.append(neighbor)
-      return order if len(order) == numCourses else []
+        order = []
+        while queue:
+          course = queue.popleft()
+          order.append(course)
+          for neighbor in graph[course]:
+            prerequisiteCount[neighbor] -= 1
+            if prerequisiteCount[neighbor] == 0:
+              queue.append(neighbor)
+        return order if len(order) == numCourses else []
+            
+        
+
+        
