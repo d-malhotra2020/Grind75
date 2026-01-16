d55474dceb1be83e24d3314768ef85a77289dd01
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-16 08:31:32 -0600

    Time: 14 ms (36.74%) | Memory: 22.7 MB (12.85%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -29,10 +29,8 @@ class Solution:
         tail = tail.next
       if list1:
         tail.next = list1
-        list1 = list1.next
       if list2:
         tail.next = list2
-        list2 = list2.next
       return dummy.next
 
 
