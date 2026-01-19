5c7986fcbf6a7358892d8d77be90edd4741dd020
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-19 10:51:00 -0600

    Time: 0 ms (100.00%) | Memory: 19.6 MB (9.09%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -1,24 +1,24 @@
 class HitCounter:
 
     def __init__(self):
-      self.hitCounterVar = []
-        
+        self.hitCounter = []
 
     def hit(self, timestamp: int) -> None:
-        self.hitCounterVar.append(timestamp)
+      self.hitCounter.append(timestamp)
+        
 
     def getHits(self, timestamp: int) -> int:
       left = 0
-      right = len(self.hitCounterVar) - 1
+      right = len(self.hitCounter) - 1
       target = timestamp - 300
       while left <= right:
-        mid = (left + right) //2
-        if self.hitCounterVar[mid] <= target:
+        mid = (left + right)//2
+        if self.hitCounter[mid] <= target:
           left = mid + 1
         else:
           right = mid - 1
-      return len(self.hitCounterVar) - left
-      
+      return len(self.hitCounter) - left
+
         
 
 
