fcac3bfa2ba51476d2b8f58ccd3a22c823dd69ff
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-01-19 08:40:22 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.25%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -4,23 +4,20 @@ class HitCounter:
         self.hits = []
 
     def hit(self, timestamp: int) -> None:
-      self.hits.append(timestamp)
-        
+        self.hits.append(timestamp)
 
     def getHits(self, timestamp: int) -> int:
       left = 0
       right = len(self.hits) - 1
       target = timestamp - 300
-
       while left <= right:
         mid = (left + right)//2
         if self.hits[mid] <= target:
           left = mid + 1
         else:
           right = mid - 1
-        
-      return len(self.hits) - left 
-        
+      return len(self.hits) - left
+
 
 
 # Your HitCounter object will be instantiated and called as such:
