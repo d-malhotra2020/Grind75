ee30fbbb4656c4d2c4c740ce4742eeb257195a14
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-02-08 15:28:09 -0600

    Time: 244 ms (57.91%) | Memory: 21.7 MB (43.25%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,22 +5,21 @@ class Solution:
         islands = 0
         rows = len(grid)
         columns = len(grid[0])
-
+      
         def dfs(r, c):
           if r < 0 or c < 0 or r >= rows or c >= columns:
             return
           if grid[r][c] != "1":
             return
-          grid[r][c] = "0" 
+          grid[r][c] = "0"
           dfs(r + 1, c)
           dfs(r - 1, c)
           dfs(r, c + 1)
           dfs(r, c - 1)
-
         for r in range(rows):
           for c in range(columns):
             if grid[r][c] == "1":
-              islands += 1
               dfs(r, c)
+              islands += 1
         return islands
-  
+            
