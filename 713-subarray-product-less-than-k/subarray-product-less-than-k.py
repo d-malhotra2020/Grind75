db215e10d53a34f5e6ef36e326fa7f59d930394a
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-02-08 18:37:50 -0600

    Time: 54 ms (73.19%) | Memory: 22.3 MB (29.08%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -1,8 +1,7 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         if k <= 1:
-          return 0 
-        
+          return 0
         left = 0
         result = 0
         product = 1
@@ -14,5 +13,3 @@ class Solution:
             left += 1
           result += (right - left) + 1
         return result
-
-            
