74e4d6f3748a981a195b850c35e5f8998a04e479
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-02-09 12:08:31 -0600

    Time: 15 ms (32.64%) | Memory: 23 MB (26.19%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -9,16 +9,17 @@ class Solution:
           return None
         while len(lists) > 1:
           mergedLists = []
+
           for i in range(0, len(lists), 2):
             list1 = lists[i]
             list2 = lists[i+1] if (i + 1) < len(lists) else None
-            mergedLists.append(self.mergeListsTogether(list1, list2))
+            mergedLists.append(self.mergedListsTogther(list1, list2))
           lists = mergedLists
         return lists[0]
 
-    def mergeListsTogether(self, list1, list2):
-      dummy = ListNode()
-      tail = dummy
+    def mergedListsTogther(self, list1, list2):
+      dummyNode = ListNode()
+      tail = dummyNode
 
       while list1 and list2:
         if list1.val < list2.val:
@@ -32,4 +33,5 @@ class Solution:
         tail.next = list1
       if list2:
         tail.next = list2
-      return dummy.next
+      return dummyNode.next
+          
