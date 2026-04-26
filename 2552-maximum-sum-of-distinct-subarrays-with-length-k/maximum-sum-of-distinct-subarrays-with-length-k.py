0f23e60c93985e6725f8e8a853574dfea16f6f0c
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-04-26 00:03:48 -0500

    Time: 111 ms (66.87%) | Memory: 37.4 MB (10.85%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,20 +1,20 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        max_sum = float('-inf')
+        current = 0
+        max_score = float('-inf')
         left = 0
-        currentSum = 0
         freq = {}
 
         for right in range(len(nums)):
-          currentSum += nums[right]
+          current += nums[right]
           freq[nums[right]] = freq.get(nums[right], 0) + 1
           if right - left + 1 == k:
-            if k == len(freq):
-              max_sum = max(max_sum, currentSum)
-            currentSum -= nums[left]
+            if len(freq) == k:
+              max_score = max(max_score, current)
+            current -= nums[left]
             freq[nums[left]] -= 1
             if freq[nums[left]] == 0:
               del freq[nums[left]]
             left += 1
-        return 0 if max_sum == float('-inf') else max_sum
-          
+        return 0 if max_score == float('-inf') else max_score
+
