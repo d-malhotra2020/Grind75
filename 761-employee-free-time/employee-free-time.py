769919069790059aaa08f6294a17d0261612be1f
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-04-30 08:36:51 -0500

    Time: 78 ms (32.42%) | Memory: 20.7 MB (85.55%) - LeetSync

---
+++ 761-employee-free-time/employee-free-time.py
@@ -0,0 +1,36 @@
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+
+      flat = []
+      merged = []
+      free_time = []
+
+      for employee in schedule:
+        for interval in employee:
+          flat.append(interval)
+
+      flat.sort(key = lambda x: x.start)
+
+      for i in flat:
+        if not merged or i.start > merged[-1].end:
+          merged.append(i)
+        else:
+          merged[-1].end = max(i.end, merged[-1].end)
+
+      for j in range(1, len(merged)):
+        start = merged[j-1].end
+        end = merged[j].start
+        if start < end:
+          free_time.append(Interval(start, end))
+      return free_time
+
+
+        
