aa3c4c1096e0c686a4bd9e61862cd026d4eb7fbb
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-05-12 20:19:26 -0500

    Time: 3 ms (69.90%) | Memory: 19.4 MB (45.70%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
         def canSplit(maxSum):
-            subArray = 1
             currentSum = 0
+            subArray = 1
+
             for n in nums:
                 if currentSum + n > maxSum:
                     subArray += 1
@@ -10,14 +11,14 @@ class Solution:
                 else:
                     currentSum += n
             return subArray <= k
-
         left = max(nums)
         right = sum(nums)
-        
+
         while left < right:
-            mid = (left + right) //2
+            mid = (left + right) // 2
             if canSplit(mid):
                 right = mid
             else:
                 left = mid + 1
         return left
+
