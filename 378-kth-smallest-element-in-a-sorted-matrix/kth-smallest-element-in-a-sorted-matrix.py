51f864d0e9a28cb6376e2754d4395fb589d89ba6
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-05-12 20:50:36 -0500

    Time: 3 ms (75.99%) | Memory: 22.7 MB (90.19%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,30 +1,29 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        def count_less_or_equal(matrix, target):
-            count = 0
+        def smallEqual(matrix, target):
             n = len(matrix)
             row = n - 1
-            column = 0
+            columns = 0
+            count = 0
 
-
-            while row >= 0 and column < n:
-                if matrix[row][column] <= target:
+            while row >= 0 and columns < n:
+                if matrix[row][columns] <= target:
                     count += row + 1
-                    column += 1
+                    columns += 1
                 else:
                     row -= 1
-            return count
-
+            return count >= k
         n = len(matrix)
         left = matrix[0][0]
-        right = matrix[n-1][n-1]
-
+        right = matrix[n - 1][n - 1]
         while left < right:
             mid = (left + right) // 2
-            count = count_less_or_equal(matrix, mid)
-            if count >= k:
+            if smallEqual(matrix, mid):
                 right = mid
             else:
                 left = mid + 1
         return left
 
+
+
+        
