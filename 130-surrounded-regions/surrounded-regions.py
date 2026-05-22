e7ba1e9933b7c15ab6573c1ec40022c5e1d317ec
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-05-21 20:25:38 -0500

    Time: 0 ms (100.00%) | Memory: 22.3 MB (78.05%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board:
+            return
+        rows = len(board)
+        columns = len(board[0])
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= columns or board[r][c] != "O":
+                return
+            board[r][c] = "S"
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for i in range(rows):
+            if board[i][0] == "O":
+                dfs(i, 0)
+            if board[i][columns - 1] == "O":
+                dfs(i, columns - 1)
+        for j in range(columns):
+            if board[0][j] == "O":
+                dfs(0, j)
+            if board[rows - 1][j] == "O":
+                dfs(rows - 1, j)
+        for i in range(rows):
+            for j in range(columns):
+                if board[i][j] == "S":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
+        return board
+            
+
