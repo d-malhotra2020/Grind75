1528a2aff6fd664f30f31729e2a4965209e2e4c4
Dhruv Malhotra <drewmalhotra2024@gmail.com>
2026-05-27 19:42:17 -0500

    Time: 774 ms (35.02%) | Memory: 41.6 MB (90.08%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -2,34 +2,35 @@ class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         if source == target:
             return 0
-        
-        busStops = {}
 
+        busStop = {}
+        
         for i, route in enumerate(routes):
             for stop in route:
-                if stop not in busStops:
-                    busStops[stop] = []
-                busStops[stop].append(i)
-
+                if stop not in busStop:
+                    busStop[stop] = []
+                busStop[stop].append(i)
+        
         visited = set()
         queue = deque()
 
-        if source not in busStops:
+        if source not in busStop:
             return -1
 
-        for bus in busStops[source]:
-            if bus not in visited:
-                queue.append((bus, 1))
-                visited.add((bus))
-
+        for bus in busStop[source]:
+            queue.append((bus, 1))
+            visited.add((bus))
+        
         while queue:
             currentBus, numChanges = queue.popleft()
+            
             for stop in routes[currentBus]:
                 if stop == target:
                     return numChanges
-
-                for busConnection in busStops[stop]:
+            
+                for busConnection in busStop[stop]:
                     if busConnection not in visited:
                         queue.append((busConnection, numChanges + 1))
                         visited.add((busConnection))
         return -1
+        
